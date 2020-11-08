80f21e306a41e55d7fef5379dff94f7bc11d438f
m199003 <m.90.j@hotmail.com>
2020-11-08 19:44:42 +0100

    Update LoginForm.js

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -5,19 +5,51 @@ class LoginForm extends React.Component {
     constructor(props) {
       super(props)
       this.state = {
-
+        identity: '', 
+        identity1: '',
+        identityConfirmation: null,
+        signinError: ''
       }
+      this.clickHandler = this.clickHandler.bind(this)
     }
 
     onSubmit = e => {
         e.preventDefault()
-        this.props.setLoggedInUser("testUser") // nur als Beispiel wie man das Userobjekt zu App hochreicht beim Submit der Form
+        
+        this.props.setLoggedInUser(this.state.identity) // nur als Beispiel wie man das Userobjekt zu App hochreicht beim Submit der Form
+    }
+
+    onChange = e => {
+      this.setState({ identity: e.target.value })
+      if (this.props.onChange) {
+        this.props.onChange()
+      }
+    }
+
+    clickHandler() {
+
+      this.setState({
+
+        identity1: 'must show up here the generated mnemonic'
+      })
     }
   
     render() {
       return (
-        <form onSubmit={this.onSubmit}>
-          <button type="submit" className="btn btn-form">Log in</button>
+        
+        
+        <form onSubmit={(e) => this.onSubmit(e)}  >
+  
+          <button type="submit" className="btn btn-large btn-primary" >Log in</button>
+          <input type="password" className="form-control small-margin" placeholder="enter your mnemonic" onChange={this.onChange} value={this.state.identity}/>
+
+
+            <div>
+              <button onClick={ () => this.clickHandler()} type="submit" className="btn btn-primary">or create one!</button>
+              <div>{this.state.identity1}</div>
+              </div>
+          
+          
         </form>
       )
     }
