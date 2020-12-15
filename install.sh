b1e5d1c65d414a7715c8776c6c7b43a26f502048
Kilian Dangendorf <kilian.dangendorf@stud.hs-hannover.de>
2020-12-15 16:44:17 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+#pull submodule
 git submodule update --init
-cd whatsdapp-lib && npm install && npm rebuild grpc --runtime=electron --target=v10.1.4 && npm run dist
-cd .. && npm install && npm install grpc --runtime=electron --target=v10.1.4
+
+#install whatsdapp-lib
+whatsdapp-dir="./whatsdapp-lib"
+#cd whatsdapp-lib && npm install && npm rebuild grpc --runtime=electron --target=v10.1.4 && npm run dist
+npm --prefix ${whatsdapp-dir} install ${whatsdapp-dir}
+npm --prefix ${whatsdapp-dir} rebuild grpc --runtime=electron --target=v10.1.4 
+npm --prefix ${whatsdapp-dir} run dist
+
+#install GUI
+#cd .. && 
+npm install 
+npm install grpc --runtime=electron --target=v10.1.4
