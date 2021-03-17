f623926b29597774abf101bdcfadc6f82f67a4b5
ganthern <nils.ganther@stud.hs-hannover.de>
2021-03-17 15:42:33 +0100

    use npm ci instead of npm install

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ git submodule update --init
 #install whatsdapp-lib
 whatsdapp_dir="./whatsdapp-lib"
 #cd whatsdapp-lib && npm install && npm rebuild grpc --runtime=electron --target=v10.1.4 && npm run dist
-npm --prefix ${whatsdapp_dir} install ${whatsdapp_dir}
+npm --prefix ${whatsdapp_dir} ci ${whatsdapp_dir}
 npm --prefix ${whatsdapp_dir} rebuild grpc --runtime=electron --target=v10.1.4 
 npm --prefix ${whatsdapp_dir} run dist
 
 #install GUI
 #cd .. && 
-npm install 
-npm install grpc --runtime=electron --target=v10.1.4
+npm ci 
+npm ci grpc --runtime=electron --target=v10.1.4
 npm rebuild grpc --runtime=electron --target=v10.1.4
